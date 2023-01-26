72390cefaffc80acb75d7a0549c684d6bec7504a
MathiasHidalgo <mejhc2307lbc@gmail.com>
2023-01-26 09:51:22 -0500

    Starting the day

--- source/sv.js
+++ source/sv.js
@@ -2,22 +2,26 @@
 const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
-const methodOverride = require('express-method-override');
+const methodOverride = require('method-override');
 const session = require('express-session');
 
 // Initializations
 const app = express()
 
 // Settings
-const port = app.set('port', process.env.PORT || 3000);
+const port = process.env.PORT || 3000;
+
 app.set('views', path.join(__dirname, 'views'));
-app.engine('.hbs', exphbs({
+
+app.engine('.hbs', exphbs.engine({
     extname: '.hbs',
     defaultLayout:'main',
     layoutsDir: path.join(app.get('views'), 'layouts'),
     partialsDir: path.join(app.get('views'), 'partials')
 }));
+
 app.set('view engine', '.hbs');
+
 // Middlewares
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
@@ -31,11 +35,14 @@ app.use(session({
 // Global Variables
 
 // Routes 
+app.use(require('./routes/home'));
+app.use(require('./routes/notes'));
+app.use(require('./routes/users'));
 
 // Static Files
 
 // Server is listening 
 
-app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`)
+app.listen(() => {
+    console.log('Server is listening on port', port)
 })
