1d99809725f46ed5e8a276fc01edef9a02eae312
MathiasHidalgo <mejhc2307lbc@gmail.com>
2023-01-27 21:42:25 -0500

    S

--- source/routes/notes.js
+++ source/routes/notes.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+
+router.get('/notes/add', (req, res) => {
+    res.render('/notes/new-note');
+})
+
 router.get('/notes', (req, res) => {
     res.send('notes from the db of your account');
 })
