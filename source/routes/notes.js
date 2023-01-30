4b66a578e29e6796042bcb18e52efcc2c91b1503
MathiasHidalgo <mejhc2307lbc@gmail.com>
2023-01-30 15:24:03 -0500

    we need to finish the form

--- source/routes/notes.js
+++ source/routes/notes.js
@@ -3,7 +3,12 @@ const router = express.Router();
 
 
 router.get('/notes/add', (req, res) => {
-    res.render('/notes/new-note');
+    res.render('notes/new-note');
+})
+
+router.post('notes/new-note', (req, res) => {
+    console.log(req.body);
+    res.send('ok');
 })
 
 router.get('/notes', (req, res) => {
