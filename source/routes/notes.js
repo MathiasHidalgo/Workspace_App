a174282f92607dcfdbd055191edbb322246565db
MathiasHidalgo <mejhc2307lbc@gmail.com>
2023-01-31 18:03:02 -0500

    notes alredy done

--- source/routes/notes.js
+++ source/routes/notes.js
@@ -2,13 +2,35 @@ const express = require('express');
 const router = express.Router();
 
 
+const Note = require('../models/Note');
+
 router.get('/notes/add', (req, res) => {
     res.render('notes/new-note');
 })
 
-router.post('/notes/new-note', (req, res) => {
-    console.log(req.body);
-    res.send('ok');
+router.post('/notes/new-note', async (req, res) => {
+    const {title, description} = req.body;
+    const errors = [];
+
+    if(!title){
+        errors.push({text: "Please write a tittle"});
+    }
+    if(!description){
+        errors.push({text: "Please write a description"});
+    }
+    if(errors.length > 0){
+        res.render('notes/new-note', {
+            errors,
+            title,
+            description
+        });
+    }
+    else {
+      const newNote = new Note({ title, description });
+      await newNote.save();
+      res.redirect('/notes')
+    }
+
 })
 
 router.get('/notes', (req, res) => {
