70d28636a340490b8d66fd01727f9aa526dfb168
MathiasHidalgo <mejhc2307lbc@gmail.com>
2023-02-07 18:53:05 -0500

    before debug

--- source/routes/notes.js
+++ source/routes/notes.js
@@ -15,12 +15,17 @@ router.post('/notes/new-note', async (req, res) => {
     const {title, description} = req.body; // the body of the request has names defined in the hbs, as title in the input box and description name defined in the texarea box
     const errors = []; // if we have errors, store them in an array
 
+    // if there aren't a title written in the form, add a document in the array with the text saying please.... and push it on the array errors
     if(!title){
         errors.push({text: "Please write a tittle"});
     }
+
+    // the same process if you don't write anything in the description form
     if(!description){
         errors.push({text: "Please write a description"});
     }
+
+    // And if there are errors, render with the alerts (rendered in new-note.hbs )
     if(errors.length > 0){
         res.render('notes/new-note', {
             errors,
@@ -28,9 +33,10 @@ router.post('/notes/new-note', async (req, res) => {
             description
         });
     }
+    // Else just store the notes form document, creating a new note with the model class created and sotre in a constant
     else {
       const newNote = new Note({ title, description });
-      await newNote.save();
+      await newNote.save(); // save the note 
       res.redirect('/notes')
     }
 
